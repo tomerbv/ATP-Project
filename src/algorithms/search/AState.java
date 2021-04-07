52ff731dc824de2b3a1709c0a6f178339d72c16e
Tomer Weitzman <tomerbv@gmail.com>
2021-04-07 14:11:06 +0300

    1.3

--- src/algorithms/search/AState.java
+++ src/algorithms/search/AState.java
@@ -1,6 +1,8 @@
 package algorithms.search;
 
-public abstract class AState {
+import java.util.Objects;
+
+public abstract class AState implements Comparable<AState>{
     double cost;
 
     public AState(){
@@ -9,4 +11,37 @@ public abstract class AState {
     public AState(double cost){
         this.cost = cost;
     }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void addCost(double cost) {
+        this.cost += cost;
+    }
+
+    @Override
+    public int compareTo(AState other) {
+        if (this.cost < other.cost) {
+            return -1;
+        }
+        if (this.cost == other.cost) {
+            return 0;
+        }
+        return 1;
+    }
+
+
+
+    @Override
+    public abstract boolean equals(Object o);
+
+    @Override
+    public abstract int hashCode();
+
+
 }
