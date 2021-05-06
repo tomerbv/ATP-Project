4f455d6e793e92ebcb3664f55283b596239ebbff
omermaim <omermaimon2@gmail.com>
2021-05-06 18:24:32 +0300

    ATP - PART2 TODO: complete ServerStrategyGenerateMaze and solutino file and configuration file

--- src/Server/ServerStrategyGenerateMaze.java
+++ src/Server/ServerStrategyGenerateMaze.java
@@ -4,10 +4,7 @@ import IO.MyCompressorOutputStream;
 import algorithms.mazeGenerators.Maze;
 import algorithms.mazeGenerators.MyMazeGenerator;
 
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 public class ServerStrategyGenerateMaze implements IServerStrategy{
 
@@ -15,17 +12,20 @@ public class ServerStrategyGenerateMaze implements IServerStrategy{
     public void applyStrategy(InputStream inFromClient, OutputStream outToClient) {
         try {
             ObjectInputStream fromClient = new ObjectInputStream(inFromClient);
-            ObjectOutputStream toClient = new ObjectOutputStream(new MyCompressorOutputStream(outToClient));
-
+            ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
+            ByteArrayOutputStream bytearray= new ByteArrayOutputStream();
+            MyCompressorOutputStream mycomp = new MyCompressorOutputStream(bytearray);
             int[] clientdimensions= (int[]) fromClient.readObject();
             MyMazeGenerator mg = new MyMazeGenerator();
             Maze toclientMaze = mg.generate(clientdimensions[0],clientdimensions[1]);
+
             //printing maze for tests only
             toclientMaze.superprint();
 
 
 
-            toClient.write(toclientMaze.toByteArray());
+            mycomp.write(toclientMaze.toByteArray());
+            toClient.writeObject();
             toClient.flush();
             fromClient.close();
             toClient.close();
